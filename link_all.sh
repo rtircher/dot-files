9e33ae54d0a4ed15bca657a009592a316ccf248e
Renaud Tircher <rtircher@thoughtworks.com>
2012-06-10 19:52:21 -0500

    Updating linker script

---
+++ link_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ls -1dA `pwd`/files/* `pwd`/files/.* | while read f; do
+  [ "$f" == `pwd`/files/. ] ||
+  [ "$f" == `pwd`/files/.. ] ||
+  [ "$f" == `pwd`/files/.git ] ||
+  ln -vsf "$f" ~/test
+done
