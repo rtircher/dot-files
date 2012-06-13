25b4234a8de6c5be3df6fa43becc28a2f51ad9a9
Renaud Tircher <rtircher@thoughtworks.com>
2012-06-12 23:15:34 -0500

    Adding custom build of emacs and script to automate its installation

--- setup.sh
+++ setup.sh
@@ -23,3 +23,17 @@ curl -L get.rvm.io | bash -s stable --ruby
 if [[ $? != 0 ]]; then
   echo "!!!!!!!!!! RVM install failed !!!!!!!!!!"
 fi
+
+echo "#### Installing emacs"
+mkdir emacs_install
+tar -xvf bin/emacs-24.1.tar.gz -C emacs_install
+cd emacs_install/emacs-24.1
+./configure --with-ns && make install
+if [[ $? != 0 ]]; then
+  mv nextstep/Emacs.app/Contents/Resources/Emacs.icns nextstep/Emacs.app/Contents/Resources/Emacs.old.icns
+  cp ../../bin/Emacs.icns nextstep/Emacs.app/Contents/Resources/
+  cp -r nextstep/Emacs.app /Developer/Applications/
+  echo "Emacs installed successfully"
+fi
+cd ../../
+rm -rf emacs_install
