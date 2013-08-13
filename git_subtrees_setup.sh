e7f80ad40990c3d6681f690f787a0fa39b1e00a2
Renaud Tircher <renaud.tircher@gmail.com>
2013-08-13 15:28:37 +0800

    Adding reference to git subtree blog post

--- git_subtrees_setup.sh
+++ git_subtrees_setup.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+# Look here for more info about git subtrees: https://blogs.atlassian.com/2013/05/alternatives-to-git-submodule-git-subtree/
+
 git remote add -f rupa-z git@github.com:rupa/z.git
 git subtree add --prefix files/.zsh/vendor/rupa-z rupa-z master --squash
--- git_subtrees_update.sh
+++ git_subtrees_update.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+# Look here for more info about git subtrees: https://blogs.atlassian.com/2013/05/alternatives-to-git-submodule-git-subtree/
+
 git fetch rupa-z master
 git subtree pull --prefix files/.zsh/vendor/rupa-z rupa-z master --squash
